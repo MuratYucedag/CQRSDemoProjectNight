7dfa61aacbfd6ac24d536591fd303ca5da2d168f
MuratYucedag <yucedagmurat23@gmail.com>
2025-08-14 22:03:21 +0300

    ...

---
+++ CQRSDemoProjectNight/CQRSPattern/Handlers/ProductHandlers/CreateProductCommandHandler.cs
@@ -0,0 +1,26 @@
+﻿using CQRSDemoProjectNight.Context;
+using CQRSDemoProjectNight.CQRSPattern.Commands.CategoryCommands;
+using CQRSDemoProjectNight.CQRSPattern.Commands.ProductCommands;
+using CQRSDemoProjectNight.Entities;
+
+namespace CQRSDemoProjectNight.CQRSPattern.Handlers.ProductHandlers
+{
+    public class CreateProductCommandHandler
+    {
+        private readonly DemoContext _context;
+        public CreateProductCommandHandler(DemoContext context)
+        {
+            _context = context;
+        }
+        public async Task Handle(CreateProductCommand command)
+        {
+            _context.Products.Add(new Product
+            {
+                ProductName = command.ProductName,
+                ProductPrice = command.ProductPrice,
+                ProductStock = command.ProductStock
+            });
+            await _context.SaveChangesAsync();
+        }
+    }
+}
